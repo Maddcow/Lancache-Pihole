c3864297c5696deeaa7919aea7a8ff9d87549313
Maddcow <37763928+Maddcow@users.noreply.github.com>
2022-09-09 17:02:07 -0400

    Add files via upload

---
+++ lancache-dns-updates.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+### Set variables, change as necessary ###
+# Username of the regular user you're using
+SYSTEMUSER=oct8l
+# Directory the git repository is synced to
+GITSYNCDIR=/home/$SYSTEMUSER/cache-domains
+# Your personalized config file from "Setting up our config.json file" step
+DNSMASQCONFIG=/home/$SYSTEMUSER/config.json
+
+# Create a new, random temp directory and make sure it was created, else exit
+TEMPDIR=$(mktemp -d)
+
+  if [ ! -e $TEMPDIR ]; then
+      >&2 echo "Failed to create temp directory"
+      exit 1
+  fi
+
+# Switch to the git directory and pull any new data
+cd $GITSYNCDIR && \
+  git pull > /dev/null 2>&1
+
+# Copy the .txt files and .json file to the temp directory
+cp `find $GITSYNCDIR -name "*.txt" -o -name cache_domains.json` $TEMPDIR
+
+# Copy the create-dnsmasq.sh script to our temp directory
+mkdir $TEMPDIR/scripts/ && \
+  cp $GITSYNCDIR/scripts/create-dnsmasq.sh $TEMPDIR/scripts/ && \
+  chmod +x $TEMPDIR/scripts/create-dnsmasq.sh
+
+# Copy the config over
+cp $DNSMASQCONFIG $TEMPDIR/scripts/
+
+# Generate the dnsmasq files with the script
+cd $TEMPDIR/scripts/ && \
+  bash ./create-dnsmasq.sh > /dev/null 2>&1
+
+# Copy the dnsmasq files
+cp -r $TEMPDIR/scripts/output/dnsmasq/*.conf /etc/dnsmasq.d/
+
+# Restart pihole-FTL
+sudo service pihole-FTL restart
+
+# Delete the temp directory to clean up files
+trap "exit 1"           HUP INT PIPE QUIT TERM
+trap 'rm -rf "$TEMPDIR"' EXIT
---
+++ lancache-pihole.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [ "$(id -un)" != "root" ]; then
+    echo " "
+    echo " We need root access for certain task..."
+    echo " Please run as 'root'."
+    echo " "
+    exit
+fi
+
+if ! systemctl is-active --quiet pihole-FTL; then
+  echo " "
+  echo " Pihole is currently not installed on this system."
+  echo " Install Pihole (https://pi-hole.net/) before running this script."
+  echo " "
+  exit
+fi
+
+echo " "
+echo "---------------------------------------------------------------"
+echo " $(date)"
+echo " Pihole DNS redirect for Lancache "
+echo "   Creating setup files... "
+
+loc=$(pwd)
+cd /home/$(logname)
+git clone https://github.com//uklans/cache-domains > /dev/null 2>&1
+
+#Get host ip address
+Hostip="""$(hostname -I | sed -r 's/[^], :[]+/"&"/g')"""
+
+#Setting up config.json file
+{
+echo '{'
+echo '  "ips": {'
+echo '          "generic":      '$Hostip
+echo '  },'
+echo '  "cache_domains": {'
+echo '          "default":      "generic"'
+echo '  }'
+echo '}'
+} >> config.json
+
+echo "   Generating cache-domains files... "
+
+          #### Automating cache-domains files
+
+cp /$loc/lancache-dns-updates.sh /root/lancache-dns-updates.sh
+cd /root
+chmod +x lancache-dns-updates.sh
+./lancache-dns-updates.sh
+
+echo "   Creating crontab job... "
+
+          #### Adding the script to a cron job
+(crontab -u $(whoami) -l; echo "30 3 * * * /bin/bash /root/lancache-dns-updates.sh" ) | crontab -u $(whoami) -
+
+sudo service cron reload > /dev/null 2>&1
+
+echo " Install Complete "
+echo " Enjoy! "
+echo "---------------------------------------------------------------"
+echo " "
